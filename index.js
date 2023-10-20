0f8f8d8804627c94de62055061b5949b0e01c264
Jacastro619 <jorgecastro619@gmail.com>
2023-10-20 16:11:27 -0700

    Added starter files and psuedocode

---
+++ index.js
@@ -0,0 +1,10 @@
+// install inqurer
+// require fs
+// import shapes.js module
+// create a questions array for inquirer
+// use inquire to prompt questions when index.js is ran
+// create an init function that will prompt the users with questions
+// Use switch to create a new instance based on the shape selected
+// When the shape is selected then we will create a new instance of that shape
+// create a function that will write to an svg file
+// in function use fs.writeFile to pass render function from shapes.js that will be the contents of the svg file
