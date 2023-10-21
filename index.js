4e275ef24a2ead276e8bafd8b46516b83120048e
Jacastro619 <jorgecastro619@gmail.com>
2023-10-20 23:54:26 -0700

    modified test to add test the text section of template literal

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // install inqurer
+const inquirer = require("inquirer");
 // require fs
+const fs = require("fs");
 // import shapes.js module
+const Shapes = require("./lib/shapes.js");
 // create a questions array for inquirer
+const questions = [];
 // use inquire to prompt questions when index.js is ran
 // create an init function that will prompt the users with questions
+function init() {}
 // Use switch to create a new instance based on the shape selected
 // When the shape is selected then we will create a new instance of that shape
 // create a function that will write to an svg file
 // in function use fs.writeFile to pass render function from shapes.js that will be the contents of the svg file
+init();
